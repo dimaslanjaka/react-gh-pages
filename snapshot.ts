13032e4f522170ee2f2cc228d1a727d3c4c3a26b
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-15 04:54:50 +0700

    update

--- snapshot.ts
+++ snapshot.ts
@@ -1,3 +1,3 @@
-import { ServerSnapshot } from 'react-prerender-it'
-
-ServerSnapshot()
+//import { ServerSnapshot } from 'react-prerender-it'
+import { ServerSnapshot } from '../src/index'
+ServerSnapshot({ source: 'build', destDir: 'tmp', registerStatic: [] })
