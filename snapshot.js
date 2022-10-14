fd84cf32ea3d40af15fb49fa09f8a2fd8497dec7
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-15 05:43:35 +0700

    add eslint prettier

--- snapshot.js
+++ snapshot.js
@@ -1,4 +1,4 @@
 //import { ServerSnapshot } from 'react-prerender-it'
-const { join } = require('path')
-const { ServerSnapshot } = require('../')
-ServerSnapshot({ source: join(__dirname, 'build'), dest: join(__dirname, 'tmp'), registerStatic: [] })
+const { join } = require('path');
+const { ServerSnapshot } = require('../');
+ServerSnapshot({ source: join(__dirname, 'build'), dest: join(__dirname, 'tmp'), registerStatic: [] });
