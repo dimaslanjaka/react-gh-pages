8319ae65b71ecfd575be70a8984c052e3018e8fc
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-15 05:48:04 +0700

    dynamic import

--- snapshot.js
+++ snapshot.js
@@ -1,4 +1,10 @@
 //import { ServerSnapshot } from 'react-prerender-it'
 const { join } = require('path');
-const { ServerSnapshot } = require('../');
+let ServerSnapshot;
+if (/dev/i.test(process.env.NODE_ENV)) {
+  ServerSnapshot = require('../').ServerSnapshot;
+} else {
+  ServerSnapshot = require('react-prerender-it').ServerSnapshot;
+}
+
 ServerSnapshot({ source: join(__dirname, 'build'), dest: join(__dirname, 'tmp'), registerStatic: [] });
