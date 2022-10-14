00ff930de34eddcd6908245f8ef399b9cc3470ce
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-15 05:50:26 +0700

    add docs

--- snapshot.js
+++ snapshot.js
@@ -1,5 +1,8 @@
 //import { ServerSnapshot } from 'react-prerender-it'
 const { join } = require('path');
+/**
+ * @type {import('react-prerender-it')['ServerSnapshot']}
+ */
 let ServerSnapshot;
 if (/dev/i.test(process.env.NODE_ENV)) {
   ServerSnapshot = require('../').ServerSnapshot;
@@ -7,4 +10,8 @@ if (/dev/i.test(process.env.NODE_ENV)) {
   ServerSnapshot = require('react-prerender-it').ServerSnapshot;
 }
 
-ServerSnapshot({ source: join(__dirname, 'build'), dest: join(__dirname, 'tmp'), registerStatic: [] });
+ServerSnapshot({
+  source: join(__dirname, 'build'),
+  dest: join(__dirname, 'tmp'),
+  registerStatic: []
+});
