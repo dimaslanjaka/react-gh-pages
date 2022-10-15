b134c36ee079b0b9cac6e373869cc7d8955418a8
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-15 07:03:03 +0700

    fix prettier and eslint

--- start.js
+++ start.js
@@ -8,7 +8,14 @@ const pkg = require('./package.json')
 const homepage = new URL(pkg.homepage)
 
 // cross-env-shell NODE_ENV=development PORT=4000 BROWSER=none DEBUG=chimera* react-scripts start
-const child = spawn('cross-env-shell', ['NODE_ENV=development', 'SITE_URL=' + homepage.origin, 'PORT=4000', 'BROWSER=none', 'react-scripts', 'start'])
+const child = spawn('cross-env-shell', [
+  'NODE_ENV=development',
+  'SITE_URL=' + homepage.origin,
+  'PORT=4000',
+  'BROWSER=none',
+  'react-scripts',
+  'start'
+])
 child.stdout.pipe(process.stdout)
 child.stderr.pipe(process.stderr)
 child.stdin.pipe(process.stdin)
