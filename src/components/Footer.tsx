f36a9308f1bcb3a95afc13da542c91ebd7cec576
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-17 22:32:16 +0700

    nofollow

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,10 @@ export function Footer() {
   return (
     <footer>
       <span>Source: </span>
-      <a href="https://github.com/dimaslanjaka/react-gh-pages">
+      <a
+        href="https://github.com/dimaslanjaka/react-gh-pages"
+        rel="nofollow noopener noreferer"
+      >
         https://github.com/dimaslanjaka/react-gh-pages
       </a>
     </footer>
